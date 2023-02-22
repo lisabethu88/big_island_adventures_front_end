6a9ff9f5df46a0ec81b0d088588592cf28b73bb1
Lisa Utsett <lisabethutsett@gmail.com>
2023-02-21 21:31:07 -0500

    modified constants

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,2 @@
-export const kBaseUrl = 'https://big-island-adventures-be.herokuapp.com'
-export const apiKey = 'KwEOp0fg9DesFJhrcSCVraGLNIvgkXHd'
+export const kBaseUrl = "https://big-island-adventures-be.herokuapp.com"
+export const apiKey = "KwEOp0fg9DesFJhrcSCVraGLNIvgkXHd"
